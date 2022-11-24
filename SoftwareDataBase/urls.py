6309e1b4958e953b6c51defbc1d46ee12137886f
ShizuiOT <vova.freeze@gmail.com>
2022-11-24 20:27:25 +0300

    corrections in some files + added api application

--- SoftwareDataBase/urls.py
+++ SoftwareDataBase/urls.py
@@ -2,12 +2,10 @@ from api.urls import path, include
 from django.contrib import admin
 from django.urls import path, include
 from db_save import views
-from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('QuerryDB/', include('db_show.urls')),
     path('api/', include('api.urls')),
-    path("docs/", SpectacularAPIView.as_view(url_name="schema"), name="swagger-ui")
 ]
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from . import views
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path('getSoftwareView/<str:Discipline>/<str:OpSys>/<str:Name>/<str:Practicum_Num>',
          views.SoftwareView.as_view(), name="Getter"),
     path('setSoftwareView/<str:Discipline>/<str:OpSys>/<str:Name>/<str:Practicum_Num>',
-         views.SoftwareView.as_view(), name="Setter")
+         views.SoftwareView.as_view(), name="Setter"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui")
 ]

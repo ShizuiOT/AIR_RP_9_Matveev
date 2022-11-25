1f0fbb5b64d6b58644dc09896b2640500d47b246
ShizuiOT <vova.freeze@gmail.com>
2022-11-26 00:30:30 +0300

    added tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+import json
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class SoftwareTests(APITestCase):
+    def test_get_correct(self):
+        url = reverse('Getter', args=["Corporate_architecture", "Windows", "Archi", '2'])
+        response = self.client.get(url)
+        self.assertEqual(json.loads(response.content), [{
+            "id": 6,
+            "Discipline": "Corporate_architecture",
+            "OpSys": "Windows",
+            "Name": "Archi",
+            "Practicum_Num": "2"
+        }])
